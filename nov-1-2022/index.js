980058c1d9b1952f4bf59e80e7e29321c647dd1c
musfiq1999 <mushfiqur15-10872@diu.edu.bd>
2022-11-02 15:40:17 +0600

    nov-1-2022

---
+++ nov-1-2022/index.js
@@ -0,0 +1,72 @@
+var myText = "Amra JavaScript Shikhbo Inshallah!";
+// console.log(myText);
+
+// this is a single-line commnet
+
+/* 
+this is
+a multiline
+comment 
+*/
+
+/**
+ * Dhaka
+ * Bogura
+ * Khulna
+ * Barishal
+ */
+
+var stName = "Rumi";
+console.log(typeof stName);
+
+var stAge = 27;
+console.log(typeof stAge);
+
+var stMStatus = false;
+console.log(typeof stMStatus);
+
+var stdData = { name: "Rumi", age: 27, single: true };
+console.log(typeof stdData);
+
+/**
+ * +
+ * -
+ * *
+ * /
+ * %
+ */
+
+/**
+ * =
+ * +=
+ * -=
+ * *=
+ * /=
+ * %=
+ */
+
+/**
+ * ==
+ * ===
+ * <
+ * >
+ * <=
+ * >=
+ * !=
+ * !==
+ */
+
+/**
+ * &&
+ * ||
+ */
+
+var num1 = 5;
+num1 += 3; //num1 = num1 + 3
+num1 -= 2;
+console.log(num1);
+
+var dip = 22;
+var khandoker = "22";
+
+console.log(dip !== khandoker);
