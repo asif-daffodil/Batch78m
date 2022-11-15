e83abb345010d54bac41c04c15989a934db2a759
Asif Abir <abir@dipti.com.bd>
2022-11-15 12:43:40 +0600

    nov-9-2022

---
+++ nov-9-2022/js/script.js
@@ -0,0 +1,17 @@
+// document.getElementById("khondoker").innerHTML += "<h1>DIPTI Choltese</h1>";
+document.querySelector("#khondoker").innerHTML += "<h1>DIPTI Choltese</h1>";
+
+console.log(document.querySelector("#khondoker"));
+
+console.log(document.getElementById("khondoker"));
+
+const yusufVai = document.getElementsByClassName("yousufVai");
+
+yusufVai[0].innerHTML = "Yusuf Vai er onek boro business!";
+yusufVai[1].innerHTML = "Yusuf Vai dhakay thake!";
+yusufVai[2].innerHTML = "Yusuf Vai lomba manush!";
+yusufVai[3].innerHTML = "Yusuf Vai er ojon kom!";
+// alert("this is an alert");
+document.write("This is a text from javascript");
+console.log(document.querySelectorAll(".yousufVai"));
+console.log(document.getElementsByClassName(".yousufVai"));
